86e57e39a29f18f7ccbee6b583ae573c3fa0c2e7
Jorge Mario Peláez Velásquez <jmpelaez@gmail.com>
2016-08-22 00:14:58 -0500

    Printing logo on receipts

--- apps/receipt/lib/receipt.ex
+++ apps/receipt/lib/receipt.ex
@@ -1,5 +1,9 @@
 defmodule Receipt do
   use Application
+# ESC/POS helping bits:
+  @esc 0x1b
+  @gs  0x1d
+  @lf  0x0a
 
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
@@ -23,31 +27,37 @@ defmodule Receipt do
     String.contains? props, "ID_MODEL=TM-T88V"
   end
 
-# ESC/POS helping bits:
-# ESC = "0x1b"
-# GS  = "0x1d"
-# NUL = "0x00"
-
   def print_ticket() do
-    #File.copy "test/logo.prn", "ticket.prn"
-    #{:ok, file} = File.open("ticket.prn", [:binary,:append])
-    {:ok, file} = File.open("ticket.prn", [:binary,:write])
-    IO.binwrite file, [0x1b,"@"] # Reset to defaults
-    IO.binwrite file, [0x1b,"a",0x01] # Centered
-    IO.binwrite file, [0x1b,"E",0x01] # Bold on
-    IO.binwrite file, ["Ozio Caff", 0x82, 0x0a]
-    IO.binwrite file, [0x1b,"E",0x00] # Bold off
-    IO.binwrite file, ["Todos merecemos unos minutos de Ozio", 0x0a]
-    cut_ticket file
     #System.cmd "png2pos", ["-c", "test/logo.png"]
-    #System.cmd "dd", ["if=ticket.prn","of=/dev/usb/#{find_printer}"]    
+    File.copy "test/logo.prn", "ticket.prn"
+    {:ok, file} = File.open("ticket.prn", [:binary,:append])
+    #{:ok, file} = File.open("ticket.prn", [:binary,:write])
+    IO.binwrite file, [@esc,"@"] # Reset to defaults
+    open_drawer file
+    IO.binwrite file, [@esc,"a",0x01] # Centered
+    #IO.binwrite file, [@esc,"E",0x01] # Bold on
+    #IO.binwrite file, ["Ozio Caff", 0x82, @lf]
+    #IO.binwrite file, [@esc,"E",0x00] # Bold off
+    IO.binwrite file, ["El caf", 0x82, " de Colombia al estilo italiano", @lf]
+    #IO.binwrite file, ["Todos merecemos unos minutos de Ozio", 0x0a]
+    cut_ticket file
+    System.cmd "dd", ["if=ticket.prn","of=/dev/usb/#{find_printer}"]    
   end
 
   def open_drawer(file) do
-    IO.binwrite file, [0x1d,0x56,0x41,0x40]
+    IO.binwrite file, [@esc,"p",0x00,25,250]
   end
 
   def cut_ticket(file) do
-    IO.binwrite file, [0x1d,0x56,0x41,0x40]
+    IO.binwrite file, [@gs,"V",0x41,0x40]
   end
+
+  def print_barcode(file, code) do
+    IO.binwrite file, [@gs,"h", 0x80] # Barcode height
+    IO.binwrite file, [@gs,"f", 0x00] # font
+    IO.binwrite file, [@gs,"H", 0x02] # number position
+    IO.binwrite file, [@gs,"k", 0x04] # print the barcode
+    IO.binwrite file, [code, 0x00, 0x0a]
+  end
+
 end
